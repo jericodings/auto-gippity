4b7903839c89058075fc2497e393fcb5dc57df84
jericodings <jericosalenga@gmail.com>
2024-02-11 14:57:57 +0800

    User Command line Interactivity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod ai_functions;
+mod apis;
+mod helpers;
+mod models;
+
+use helpers::command_line::get_user_response;
+
+#[tokio::main]
+async fn main() {
+    let usr_req: String = get_user_response("What webserver are we building today?");
+
+    dbg!(usr_req);
 }
