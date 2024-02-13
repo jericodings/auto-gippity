20fcae2ec0faa6b960a0f2ddadad58a51b21fb87
jericodings <jericosalenga@gmail.com>
2024-02-13 21:58:11 +0800

    Some Error Handlings

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -6,7 +6,7 @@ use std::env;
 use reqwest::header::{HeaderMap, HeaderValue};
 
 // Call Large Language Model (i.e. GPT-4)
-pub async fn call_gpt(messages: Vec<Message>) {
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
     dotenv().ok();
 
     // Extract API Key information
@@ -24,20 +24,22 @@ pub async fn call_gpt(messages: Vec<Message>) {
     // Create api key header
     headers.insert(
         "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?
     );
 
     // Create Open AI Org header
     headers.insert(
         "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap()
+        HeaderValue::from_str(api_org.as_str())
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?
     );
 
     // Create client
     let client: Client = Client::builder()
         .default_headers(headers)
         .build()
-        .unwrap();
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
     // Create chat completion
     let chat_completion: ChatCompletion = ChatCompletion {
@@ -46,15 +48,16 @@ pub async fn call_gpt(messages: Vec<Message>) {
         temperature: 0.1
     };
 
-    // Troubleshooting
-    let res_raw = client
-      .post(url)
-      .json(&chat_completion)
-      .send()
-      .await
-      .unwrap();
-    dbg!(res_raw.text().await.unwrap());
+    // // Troubleshooting
+    // let res_raw = client
+    //   .post(url)
+    //   .json(&chat_completion)
+    //   .send()
+    //   .await
+    //   .unwrap();
+    // dbg!(res_raw.text().await.unwrap());
 
+    Ok("some_string".to_string())
 }
 
 
