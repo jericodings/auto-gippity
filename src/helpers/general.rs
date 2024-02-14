793efb3a17d3e50685c1d56c19d81ee3353db6d3
jericodings <jericosalenga@gmail.com>
2024-02-14 19:19:04 +0800

    Extended the AI Function to encourage desired output

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,14 +1,25 @@
-use crate::apis::call_request::call_gpt;
 use crate::models::general::llm::Message;
-use reqwest::Client;
-use serde::de::DeserializeOwned;
-use std::fs;
 
 // Extend ai function to encourage specific output
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) {
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str)  -> Message {
     let ai_function_str: &str = ai_func(func_input);
 
+    // Extend the string to encourage only printing the output
+    let msg: String = format!(
+        "FUNCTION: {}
+  INSTRUCTION: You are a function printer. You ONLY print the results of functions.
+  Nothing else. No commentary. Here is the input to the function: {}.
+  Print out what the function will return.",
+        ai_function_str, func_input
+    );
+
+    // Return message
+    Message {
+        role: "system".to_string(),
+        content: msg
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -17,11 +28,9 @@ mod tests {
 
     #[test]
     fn tests_extending_ai_function() {
-
-        let x_str = convert_user_input_to_goal("dummy variable");
-
-        dbg!(x_str);
-
+        let extended_msg: Message =
+            extend_ai_function(convert_user_input_to_goal, "dummy variable");
+        dbg!(&extended_msg);
+        assert_eq!(extended_msg.role, "system".to_string());
     }
-
 }
