add0f1a19a6e54f5e75509544e2edccaa7b43770
jericodings <jericosalenga@gmail.com>
2024-02-15 08:41:31 +0800

    Local File Functions

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -5,16 +5,17 @@ use reqwest::Client;
 use serde::de::DeserializeOwned;
 use std::fs;
 
-const CODE_TEMPLATE_PATH: &str = "/home/jerico/rust_autogpt/web_template/src/code_template.rs";
+const CODE_TEMPLATE_PATH: &str =
+    "/home/jerico/rust_autogpt/web_template/src/code_template.rs";
 
-pub const WEB_SERVER_PROJECT_PATH: &str = "/home/jerico/rust_autogpt/web_template";
+pub const EXEC_MAIN_PATH: &str =
+    "/home/jerico/rust_autogpt/web_template/src/main.rs";
 
-pub const EXEC_MAIN_PATH: &str = "/home/jerico/rust_autogpt/web_template/src/main.rs";
-
-const API_SCHEMA_PATH: &str = "/home/jerico/rust_autogpt/auto_gippity/schemas/api_schema.json";
+const API_SCHEMA_PATH: &str =
+    "/home/jerico/rust_autogpt/auto_gippity/schemas/api_schema.json";
 
 // Extend ai function to encourage specific output
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str)  -> Message {
     let ai_function_str: &str = ai_func(func_input);
 
     // Extend the string to encourage only printing the output
@@ -29,7 +30,7 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
     // Return message
     Message {
         role: "system".to_string(),
-        content: msg,
+        content: msg
     }
 }
 
@@ -55,7 +56,7 @@ pub async fn ai_task_request(
         Ok(llm_resp) => llm_resp,
         Err(_) => call_gpt(vec![extended_msg.clone()])
             .await
-            .expect("Failed twice to call OpenAI"),
+            .expect("Failed twice to call OpenAI")
     }
 }
 
@@ -82,12 +83,6 @@ pub async fn check_status_code(client: &Client, url: &str) -> Result<u16, reqwes
 // Get Code Template
 pub fn read_code_template_contents() -> String {
     let path: String = String::from(CODE_TEMPLATE_PATH);
-    fs::read_to_string(path).expect("Failed to read code template")
-}
-
-// Get Exec Main
-pub fn read_exec_main_contents() -> String {
-    let path: String = String::from(EXEC_MAIN_PATH);
     fs::read_to_string(path).expect("Failed to read code template")
 }
 
